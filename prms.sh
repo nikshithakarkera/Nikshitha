3ef5900f489a161f15a88c70274eb115b53b6520
nikshithakarkera <98947414+nikshithakarkera@users.noreply.github.com>
2022-02-23 13:47:34 +0530

    Add files via upload

---
+++ prms.sh
@@ -0,0 +1,26 @@
+echo -n "enter the first file:"
+read file1
+if [ -e $file1 ]
+then
+set -- `ls -ld $file1`
+file1perm=$1
+else
+echo "File doesnt exist"
+exit
+fi
+echo -n "enter the second file:"
+read file2
+if [ -e $file2 ]
+then
+set -- `ls -ld $file2`
+file2perm=$1
+fi
+if [ "$file1perm" = "$file2perm" ]
+then
+echo "File permissions are identical"
+echo "Permission is $file1perm"
+else
+echo "File permission are not identical"
+echo "$file1 permission is $file1perm"
+echo "$file2 permission is $file2perm"
+fi
