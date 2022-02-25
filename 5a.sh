2421de836a7ee8443e6ec96a86f827dcd2704916
nikshithakarkera <98947414+nikshithakarkera@users.noreply.github.com>
2022-02-25 15:49:36 +0530

    Delete 5a.sh

--- 5a.sh
+++
@@ -1,9 +0,0 @@
-echo -n "enter the filename:"
-read filename
-if [ -e $filename ]
-then 
-set -- `ls -ld $filename`
-echo "The creation time of $filename is $6 $7 $8"
-else
-echo "File doesnt exist"
-fi
