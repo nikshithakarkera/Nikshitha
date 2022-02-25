ab817f8250a1cf7f3480f587c046edf8b9be13f1
nikshithakarkera <98947414+nikshithakarkera@users.noreply.github.com>
2022-02-25 16:38:38 +0530

    Add files via upload

---
+++ Shellscript/7a.sh
@@ -0,0 +1,26 @@
+user=`whoami`
+set -- `who`
+for i in $*
+do
+user1=$1
+if [ $user = $user1 ]
+then
+set -- `echo $4 | tr ":" " "`
+h=$1
+if [ $h -ge 4 ] && [ $h -lt 12 ]
+then
+echo "Good Morning Mr/Mrs. $user"
+elif [ $h -ge 12 ] && [ $h -lt 16 ]
+then
+echo "Good Afternoon Mr/Mrs. $user"
+elif [ $h -ge 16 ] && [ $h -lt 19 ]
+then
+echo "Good Evening Mr/Mrs. $user"
+else
+echo "Good Night Mr/Mrs. $user"
+fi
+exit
+else
+shift 5
+fi
+done
