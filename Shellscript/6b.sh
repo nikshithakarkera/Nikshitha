8fe6c3b41ef6d20b5121188ab3b55ff889fcc78d
nikshithakarkera <98947414+nikshithakarkera@users.noreply.github.com>
2022-02-25 14:42:46 +0530

    Update 6b.sh

--- Shellscript/6b.sh
+++ Shellscript/6b.sh
@@ -1,3 +1,5 @@
+#6b.Write a shellscript to list all the files in a directory whose filename is 
+#atleast 10 characters.(use expr command to check the length) 
 for i in `ls`
 do
 fl=`expr length $i`
@@ -6,4 +8,3 @@ then
 echo $i
 fi
 done
-
