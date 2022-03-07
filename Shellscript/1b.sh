9cd732e55bfe1d9a0576eefe52a032e717b59ac0
nikshithakarkera <98947414+nikshithakarkera@users.noreply.github.com>
2022-03-07 15:43:08 +0530

    Update 1b.sh

--- Shellscript/1b.sh
+++ Shellscript/1b.sh
@@ -1,5 +1,5 @@
 #1b.Write a shellscript that accepts a pathname and creates all the components in that pathname as directories.
-#Forexample, if the scriptis named mpc,then the command mpc a/b/c/d should create directories a,a/b,a/b/c,a/b/c/d. 
+#For example, if the scriptis named mpc,then the command mpc a/b/c/d should create directories a,a/b,a/b/c,a/b/c/d. 
 if [ $# -ne 0 ]
 then
 p=`echo $1 | tr "/" " "`
