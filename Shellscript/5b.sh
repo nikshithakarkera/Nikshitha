c79ce4c09a506d806c13b9b9dfd45a894cb31ad5
nikshithakarkera <98947414+nikshithakarkera@users.noreply.github.com>
2022-05-02 16:08:10 +0530

    Add files via upload

---
+++ Shellscript/5b.sh
@@ -0,0 +1,12 @@
+#5b. Write a shellscript to display the calendar for current month with current date replaced by
+#*or** depending on whether the date has one digit or two digits 
+
+date=`date +%d`
+date=`expr $date + 0`
+l=`cal | tr -d "[\137][\010]" | grep -wn "$date" | head -c 1`
+if [ $date -gt 9 ]
+then
+	cal | tr -d "[\137][\010]" | sed "$l s/$date/**/"
+else
+	cal | tr -d "[\137][\010]" | sed "$l s/$date/*/"
+fi
