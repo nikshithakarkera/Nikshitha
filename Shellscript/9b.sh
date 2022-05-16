757bf74e75c83848bd8f543fc5112fd8191a1677
NIKSHI <98947414+nikshithakarkera@users.noreply.github.com>
2022-05-16 14:40:59 +0530

    Update 9b.sh

--- Shellscript/9b.sh
+++ Shellscript/9b.sh
@@ -1,5 +1,9 @@
-echo -n "Enter a file name:"
-read filename
+#9b. Write a shell script that folds long lines into 40 columns. Thus any line that exceeds 40
+#characters must be broken after 40th, a “/” is to be appended as the indication of folding and
+#processing is to be continued with the residue. The input is to be supplied through a text file
+#created by the user.echo -n "Enter a file name:"
+#read filename
+
 if [ -f $filename ]
 then
 n=40
@@ -27,4 +31,4 @@ while read line
   done <$filename
  else
    echo "$filename FILE NOT FOUND"
-fi             
+fi            
