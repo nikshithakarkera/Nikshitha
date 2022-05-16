59d32ec9f1b0d4a90177def6ae8871fb22547558
NIKSHI <98947414+nikshithakarkera@users.noreply.github.com>
2022-05-16 16:47:19 +0530

    Update 7a.sh

--- Shellscript/7a.sh
+++ Shellscript/7a.sh
@@ -1,5 +1,6 @@
 #7a.Write a shellscript that gets executed and displays the message either "Good morning" or 
 #"Good afternoon" or "Good evening" depending upon time at which the user logs in.
+
 user=`whoami`
 set -- `who`
 for i in $*
